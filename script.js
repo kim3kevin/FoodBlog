3fb6ffcfbfbc3a792e893e771017aa59d2176b16
Sang Hyun Kim <kim3kevin@gmail.com>
2017-08-07 16:52:04 +0900

    deleted commented out stuff

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-/*$(document).ready(function() {
-    $(window).scroll(function() {
-        if($(this).scrollTop() > 100){
-            $('#up').stop().animate({
-                top: '500px'    
-                }, 500);
-        }
-        else{
-            $('#up').stop().animate({
-               top: '-100px'    
-            }, 500);
-        }
-    });
-    $('#up').click(function() {
-        $('html, body').stop().animate({
-           scrollTop: 0
-        }, 500, function() {
-           $('#up').stop().animate({
-               top: '-100px'    
-           }, 500);
-        });
-    });
-});    */
-
 $(document).ready(function(){
   $(window).scroll(function(){
     if($(this).scrollTop() > 100){
